fd05844fd4f8a877d31fe74e125c418a147e8fb9
Alona Puhach <alen1414@gmail.com>
2021-12-26 14:26:54 -0400

    tasa/one

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,47 @@
 // Add imports above this line
+import simpleLightbox from 'simplelightbox';
+
+import '../../node_modules/simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+
 console.log(galleryItems);
+
+
+const paletteContainer = document.querySelector('.gallery');
+const cardsMarkup = createCardsMarkup(galleryItems);
+
+console.log(createCardsMarkup('galleryItems'));
+
+
+
+function createCardsMarkup(cards) {
+  return galleryItems.map(({ preview, original, description }) => {
+      return `
+      
+ <li class="gallery__item">
+  <a class="gallery__item" 
+  href="${original}">
+  <img class="gallery__image" 
+  src="${preview}" 
+  alt="${description}" />
+</a>
+</li>
+    `;
+    })
+    .join('');
+};
+paletteContainer.insertAdjacentHTML('beforeend', cardsMarkup)
+
+let lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionPosition:'bottom',
+    captionDelay:250,
+});
+ 
+
+lightbox.on(() => {
+  lightbox.next();
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,39 @@
 
+import Player from'@vimeo/player';
+import throttle from "lodash/throttle";
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+ console.log(iframe);
+ console.log(player);
+
+
+const idPlayer = new Vimeo.Player('vimeo-player');
+
+
+// const onPlay = function(idPlayer) {
+//     {
+//         duration: 61.857
+//         percent: 0
+//         seconds: 0
+//     }
+    
+//     // data is an object containing properties specific to that event
+// };
+
+// player.on('timeupdate', throttle () => {
+//     player.getTime().then((sec)=>{
+//         console.log(sec);
+
+//     localStorage.setItem("videoplayer-current-time",JSON.stringify(sec));
+// }),
+// },1000);
+
+// const startTime= 
+// JSON.parse(localStorage.getItem('videoplayer-current-time')) || 0;
+// console.log(startTime);
+// player.getTime(startTime);
+
+//     function videoStart(startTime){
+//         console.log(startTime);
+//     }
