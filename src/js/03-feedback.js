066ddb65fa951827a1314936b736f55e1d176863
Alona Puhach <alen1414@gmail.com>
2021-12-27 15:29:33 -0400

    task/third

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
 
+import throttle from "lodash.throttle";
+// const throttle = require('lodash.throttle');
+const STORAGE_KEY ='fedback-msg';
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+   input: document.querySelector('.feedback-form input')
+
+};
+console.log(refs.form);
+console.log(refs. textarea);
+console.log(refs. input);
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onTextareaInput,500));
+populateTextarea();
+
+function onFormSubmit(evt){
+evt.preventDefault();
+console.log('отправляем форму');
+evt.currentTarget.reset();
+localStorage.removeItem('STORAGE_KEY');
+};
+
+
+function onTextareaInput(evt){
+    const message = evt.currentTarget.value;
+    console.log(message);
+    localStorage.setItem('STORAGE_KEY', message);
+};
+
+function populateTextarea(){
+    const savedMessage = localStorage.getItem('STORAGE_KEY');
+    if (savedMessage){
+        console.log(savedMessage);
+        refs.textarea.value=savedMessage;
+    }
+};
