dd602c3fe0569fa2df9b61ff79fa1d170e3cf98a
Alona Puhach <alen1414@gmail.com>
2021-12-29 13:53:49 -0400

    task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,45 +1,57 @@
 
 import throttle from "lodash.throttle";
 // const throttle = require('lodash.throttle');
-const STORAGE_KEY ='fedback-msg';
-const formData={};
-
-const refs = {
-    form: document.querySelector('.feedback-form'),
-    textarea: document.querySelector('.feedback-form textarea'),
-   input: document.querySelector('input', e=>{
-       console.log(e.target);
-    // formData[e.target.name]= e.target.value;
-    
-});
-
-};
-console.log(refs.form);
-console.log(refs. textarea);
-console.log(refs. input);
-
-refs.form.addEventListener('submit', onFormSubmit);
-refs.textarea.addEventListener('input', throttle(onTextareaInput,500));
-populateTextarea();
-
-function onFormSubmit(evt){
-evt.preventDefault();
-console.log('отправляем форму');
-evt.currentTarget.reset();
-localStorage.removeItem('STORAGE_KEY');
-};
 
 
-function onTextareaInput(evt){
-    const message = evt.target.value;
-    console.log(message);
-    localStorage.setItem('STORAGE_KEY', message);
-};
 
-function populateTextarea(){
-    const savedMessage = localStorage.getItem('STORAGE_KEY');
-    if (savedMessage){
-        console.log(savedMessage);
-        refs.textarea.value=savedMessage;
-    }
-};
+
+const STORAGE_KEY = "feedback-form-state";
+
+const ref = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea')
+}
+
+const { form, input, textarea } = ref;
+
+saveValue();
+form.addEventListener('input', throttle(onInputForm, 500));
+form.addEventListener('submit',onSubmitForm)
+
+
+function onInputForm() { 
+ let feedbackForm  = {
+  email: input.value,
+  textarea: textarea.value
+  };
+ localStorage.setItem(STORAGE_KEY,JSON.stringify(feedbackForm))
+}
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  
+  const inputValueEmail = e.currentTarget.elements.email.value;
+  const textArea = textarea.value;
+
+  const options = {
+    email: inputValueEmail,
+    textarea: textArea
+  };
+  console.log(options);
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+}
+
+function saveValue() { 
+  const saveData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (saveData) { 
+    input.value = saveData.email;
+    textarea.value = saveData.textarea;
+  }
+}
+
+
+
+
+
